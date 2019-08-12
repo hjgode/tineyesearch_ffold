9e221e026a0d31f2b4404f85a60c75d18cb79267
hjgode <hjgode@gmail.com>
2019-08-12 16:09:03 +0200

    added options

--- tineyesearch.js
+++ tineyesearch.js
@@ -1,9 +1,22 @@
 var contextMenu = require("sdk/context-menu");
 var windows = require("sdk/windows");
+var preferences = require("sdk/simple-prefs").prefs;
+
 
 
 function onUpdated(tab) {console.log("Updated tab: ${tab.id}");} 
 function onError(error) { console.log("Error: ${error}"); } 
+
+function onPrefChange(prefName) {
+  console.log("The preference " + 
+              prefName + 
+              " value has changed!");
+}
+require("sdk/simple-prefs").on("somePreference", onPrefChange);
+require("sdk/simple-prefs").on("someOtherPreference", onPrefChange);
+
+// `""` listens to all changes in the extension's branch
+require("sdk/simple-prefs").on("", onPrefChange);
 
 var menuItem = contextMenu.Item({
   label: "Search at TinEye",
@@ -15,6 +28,9 @@ var menuItem = contextMenu.Item({
     
 	onMessage: function (selectionText) {
 		console.log("onMessage: " + selectionText);
+		var sortType = require('sdk/simple-prefs').prefs['TineyeSortType'];
+		var sortOrder = require('sdk/simple-prefs').prefs['TineyeSortOrder'];
+		var showResultAs = require('sdk/simple-prefs').prefs['ShowResultAs'];
 //		let tabs = require("sdk/tabs");
 //		tabs.create("https://tineye.com/search/?pluginver=firefox-1.4.1&sort=size&order=desc&url=" + selectionText);
 		if (window === null || typeof window !== "object") {
@@ -30,7 +46,51 @@ var menuItem = contextMenu.Item({
 			//*/
 			console.log("window=" + window);
 		}
-		//OK: window.open("https://tineye.com/search/?pluginver=firefox-1.4.1&sort=size&order=desc&url=" + selectionText);
-		window.gBrowser.selectedTab =  window.gBrowser.addTab("https://tineye.com/search/?pluginver=firefox-1.4.1&sort=size&order=desc&url=" + selectionText);
-  }
+		var query_string = "";
+		switch(sortType) {
+			case "score":
+				query_string = "&sort=score";
+				break;
+			case "size":
+				query_string = "&sort=size";
+				break;
+			case "crawl_date":
+				query_string = "&sort=crawl_date";
+				break;
+			default:
+				query_string = "&sort=crawl_date";
+				sortType="size";
+		}
+		switch(sortOrder) {
+			case "asc":
+				query_string += "&order=asc";
+				break;
+			case "desc":
+				query_string += "&order=desc";
+				break;
+			default:
+				query_string += "&order=desc";
+				sortOrder="desc";
+		}
+		console.log("sortType: " + sortType);
+		console.log("sortOrder: " + sortOrder);
+		console.log("showResultAs: " + showResultAs);
+		var newUrl = "https://tineye.com/search/?pluginver=firefox-1.4.1" + query_string + "&url=" + selectionText;
+		console.log("URL="+newUrl);
+		// "https://tineye.com/search/?pluginver=firefox-1.4.1&sort=size&order=desc&url=" + selectionText);
+		switch(showResultAs){
+			case "foreground":
+				window.gBrowser.selectedTab =  window.gBrowser.addTab(newUrl); 
+				break;
+			case "background":
+				window.gBrowser.addTab(newUrl); 
+				break;
+			case "current":
+				window.gBrowser.loadURI(newUrl);
+				break;
+			default:
+				window.gBrowser.selectedTab =  window.gBrowser.addTab(newUrl);
+				showResultAs="foreground";
+		}
+    }
 });
